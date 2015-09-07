171e56363167cabe28a8a12018d858c635c475d7
Andy Cheng <ccjeng@gmail.com>
2015-09-07 12:58:22 +0800

    no message

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 **台北丟垃圾（網頁版）**
 -----------------
-[網站] (http://TaipeiDisaster.ccjeng.com/) 
+[網站] (http://ccjeng.github.io/TPETrashCan/) 
 
 **資料來源**
-臺北市政府 環保局 行人專用清潔箱 (http://data.taipei/opendata/datalist/datasetMeta;jsessionid=D8C7C78B3B954689A9AE36A513A02315?oid=a835f3ba-7f50-4b0d-91a6-9df128632d1c)
+[臺北市政府 環保局 行人專用清潔箱] (http://data.taipei/opendata/datalist/datasetMeta;jsessionid=D8C7C78B3B954689A9AE36A513A02315?oid=a835f3ba-7f50-4b0d-91a6-9df128632d1c)
 
 **License**
-[CC0] (http://creativecommons.tw/cc0)
+[MIT] (http://ccjeng.mit-license.org/)
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 
 //initialize
-Parse.$ = jQuery;
+//Parse.$ = jQuery;
 Parse.initialize('5fzYdG6YMpMPKBNSqvzhEL1OVoXgcVvlCAghW09Q', 'NK9ycOzWNik9d6axOAndjYXwRuycfX2qTIyidyxV');
 
 //Get user location HTML5 >> if no location, set default location
+
 
 var map;
 function initMap() {
@@ -18,58 +19,29 @@ function initMap() {
 
 }
 
-/*
-var TrashCan = Parse.Object.extend('TPE201509');
 
-
-// Create a query for places
-var query = new Parse.Query(TrashCan);
-
-// User's location
-var userGeoPoint = new Parse.GeoPoint(25.0339031 ,121.5645098);
-
-
-// Interested in locations near user.
-query.withinKilometers('location', userGeoPoint, 100);
-
-// Limit what could be a lot of points.
-query.limit(3000);
-
-// Final list of objects
-query.find({
-  success: function(results) {
-    console.log(results.length);
-
-    for (var i = 0; i < results.length; ++i){
-      //Draw map
-      addMarker(results[i]);
-
-     }
-  },
-  error: function(error) {
-         console.log('Error: ' + error.code + ' ' + error.message);
-  }
-});
-*/
-
+var mc;
 var result = [];
-      var processCallback = function(res) {
+var processCallback = function(res) {
                 result = result.concat(res);
                 if (res.length == 1000) {
                   process(res[res.length-1].id);
                   return;
                 }   
-                console.log(result.length);
-                
-                //To print all the ids
+                //console.log(result.length);
+
+                //To print all results
                 for(var i=0;i<result.length;i++){
                         //console.log(result[i].id);
                         addMarker(result[i]);
                 }
-        }
 
-        var process = function(skip) {
-            var query = new Parse.Query("TPE201509");
+                mc = new MarkerClusterer(map, markers, {gridSize: 80, maxZoom: 16});
+
+}
+
+var process = function(skip) {
+    var query = new Parse.Query("TPE201509");
 
             if (skip) {
               query.greaterThan("objectId", skip);
@@ -77,11 +49,14 @@ var result = [];
             query.limit(1000);
             query.ascending("objectId");
             query.find().then(function querySuccess(res) {
-              processCallback(res);
+                processCallback(res);
             }, function queryFailed(error) {
-                });
-          }
-      process(false);
+                console.log('Error: ' + error.code + ' ' + error.message);
+            });
+}
+process(false);
+
+//
 
 /*
 google.maps.InfoWindow.prototype.isOpen = function(){
@@ -114,4 +89,5 @@ var addMarker = function(data){
       this.infowindow.open(map, marker);
   });
 };
+
 
