8a4d5ae4fe75f2f754ff8e0a7383c5f8646373ba
andycheng <ccjeng@gmail.com>
2015-11-29 19:27:18 +0800

    1. update trashcan data 2. update geomaker js file path

--- src/js/main.js
+++ src/js/main.js
@@ -99,7 +99,7 @@ var processCallback = function(res) {
 }
 
 var process = function(skip) {
-    var query = new Parse.Query("TPE101815");
+    var query = new Parse.Query("TPE112515");
 
             if (skip) {
               query.greaterThan("objectId", skip);
